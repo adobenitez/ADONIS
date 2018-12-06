415918f7cd02b2da6b66eb03c8c9e36ea65abf00
Adonis Benitez <adonis.b3.lil_@hotmail.com>
2018-12-05 21:14:14 -0500

    TAREA

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vectores();
+            Console.ReadKey();
+        }
+        public static void Vectores()
+
+        {
+
+            int[] vec = new int[10];
+
+            int mayor = vec[0], menor = vec[0];
+
+
+            for (int i = 0; i < vec.Length; i++)
+
+            {
+
+                while (vec[i] < 1 || vec[i] > 1000)
+
+                {
+
+                    Console.WriteLine("Ingrese valor {0} del vector", i + 1);
+
+                    vec[i] = Int32.Parse(Console.ReadLine());
+
+                }
+
+
+            }
+
+            for (int i = 0; i < vec.Length; i++)
+
+            {
+
+                if (vec[i] > mayor)
+
+                {
+
+                    mayor = vec[i];
+
+                }
+
+            }
+
+            menor = mayor;
+
+            for (int i = 0; i < vec.Length; i++)
+
+            {
+
+                if (vec[i] < menor)
+
+                {
+
+                    menor = vec[i];
+
+                }
+
+            }
+
+
+
+            Console.WriteLine("El numero mayor es: " + mayor);
+
+            Console.WriteLine("El numero menor es: " + menor);
+
+        }
+    }
+}
